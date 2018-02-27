1079eed67d935f9b8670c0ced825bcc16d2a9bdc
gahag <gabriel.s.b@live.com>
2018-02-27 12:10:36 -0300

    Switched from i3 to KDE.
    Updated xinitrc.
    Updated install script.
    Removed xmodmap.
    Removed .gitignore.
    Removed i3, rofi and polybar configs.
    Removed breeze.theme config.
    Removed package list: all packages no longer used.
    Removed wallpaper changing script.

--- wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wallpaper=~/.config/wallpaper.png
-
-[[ -f ~/.config ]]    && mv -i ~/.config    ~/.config.bak
-[[ -e "$wallpaper" ]] && mv -i "$wallpaper" "$wallpaper.bak"
-
-convert -format png "$@" "$wallpaper"
